a42942548077b78d8fe9011575b3d4dd6f940e90
hyeJune <chj950807@naver.com>
2023-10-19 13:08:31 +0900

    yarn update

--- src/styles/base/Variable.style.ts
+++ src/styles/base/Variable.style.ts
@@ -2,9 +2,11 @@ import { red } from "@mui/material/colors";
 
 /*
  * 색상 파렛트
- * https://mui.com/material-ui/customization/color/
+ * https://mui.com/material-ui/customization/color/ 
  * 색상 추가해서 사용.
  */
+
+
 
 /* Red */
 
@@ -25,3 +27,4 @@ export const color_invalid = red["A700"];
 /* Cyan */
 
 /* Teal */
+
