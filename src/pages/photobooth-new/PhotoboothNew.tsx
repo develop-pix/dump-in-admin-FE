3220150966b81ae9a239729ec0622feef8ddf849
resetmerlin <resetmerlin@gmail.com>
2023-12-30 13:02:29 +0900

    Refactor: photobooth-new page를 한 page의 모듈로 Export

---
+++ src/pages/photobooth-new/PhotoboothNew.tsx
@@ -0,0 +1,9 @@
+import PhotoboothNewForm from "../../components/photobooth-new/PhotoboothNewForm";
+
+export default function BranchNew() {
+  return (
+    <div>
+      <PhotoboothNewForm />
+    </div>
+  );
+}
