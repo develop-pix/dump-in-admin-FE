de939422d7990e0bcdc64691601173467b246efe
resetmerlin <resetmerlin@gmail.com>
2023-12-30 13:03:01 +0900

    Refactor: push-message page를 한 page의 모듈로 Export

---
+++ src/pages/push-message/PushMessage.tsx
@@ -0,0 +1,9 @@
+import PushMessageForm from "../../components/push-message/PushMessageForm";
+
+export default function PushMessage() {
+  return (
+    <div>
+      <PushMessageForm />
+    </div>
+  );
+}
