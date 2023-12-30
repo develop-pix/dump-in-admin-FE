cc2785ce47a25d35fe4e2ec80f7728564d825cfe
resetmerlin <resetmerlin@gmail.com>
2023-12-30 13:10:35 +0900

    Refactor: 파일 위치를 새 폴더 안으로 전환

--- src/pages/PushMessage.tsx
+++
@@ -1,9 +0,0 @@
-import PushMessageForm from "../components/push-message/PushMessageForm";
-
-export default function PushMessage() {
-  return (
-    <div>
-      <PushMessageForm />
-    </div>
-  );
-}
