92400cebe2edaae8a3bd7288a32742c9b60cc100
resetmerlin <resetmerlin@gmail.com>
2023-12-30 13:03:33 +0900

    Refactor: review-manage page를 한 page의 모듈로 Export

---
+++ src/pages/review-manage/ReviewManage.tsx
@@ -0,0 +1,10 @@
+import ReviewManageForm from "../../components/review-manage/ReviewManageForm";
+
+// 리뷰 관리 페이지
+export default function ReviewManage() {
+  return (
+    <div>
+      <ReviewManageForm />
+    </div>
+  );
+}
