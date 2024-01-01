b0ffcb6a77b5868702d82c413574ace2cbf17c66
resetmerlin <resetmerlin@gmail.com>
2024-01-01 12:49:32 +0900

    Feat: Login form 컴포넌트에 있는 비즈니스 로직 부모 컴포넌트로 위치 변경 및 새 form 추가
    기존에는 setEmail, setPassword로 form validation을 처리했다면 지금은 type safe하게 schema validation tool인 zod와 form 용 라이브러리인 react hook form을 사용하여 작업

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,45 +1,20 @@
-import LongInput from "../reuse/input/LongInput";
-import LoginButton from "../reuse/button/LoginButton";
-import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
+import { UseFormReturn } from "react-hook-form";
+import { LoginInput } from "../../interface/reuse/Input.interface";
 import { customColors } from "../../styles/base/Variable.style";
-import { useNavigate } from "react-router-dom";
 
-export default function LoginForm() {
-  const navigate = useNavigate();
-
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [invalid, setInvalid] = useState<boolean>(false);
-
-  /*로그인 버튼 입력시*/
-  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      console.log(e);
-      console.log(email);
-      console.log(password);
-
-      //catch 문안으로 들어가야하나 build시 에러 발생하여 임의로 추가, API 적용후 아래 삭제할것
-      setInvalid(true);
-      //if 문안으로 들어가야하나 build시 에러 발생하여 임의로 추가, API 적용후 아래 삭제할것
-      navigate("/dashboard");
-
-      /*
-      로그인 API 연동
-      let login_result = await 로그인시도(email, password);
-
-      if (login_result.결과코드 === 200) {
-        대쉬보드 페이지로 이동?
-      }else{
-        setInvalid(true);
-      }
-      */
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+interface IProps {
+  onSubmitHandler: (data: LoginInput) => void;
+  register: UseFormReturn<LoginInput>["register"];
+  handleSubmit: UseFormReturn<LoginInput>["handleSubmit"];
+  inputErrors: UseFormReturn<LoginInput>["formState"]["errors"];
+}
+export default function LoginForm({
+  onSubmitHandler,
+  register,
+  handleSubmit,
+  inputErrors,
+}: IProps) {
   return (
     <Box
       sx={{
@@ -54,28 +29,49 @@ export default function LoginForm() {
           width: 500,
         }}
       >
-        <form onSubmit={onSubmitHandler}>
-          <LongInput
-            label="ID"
-            type="email"
-            placeholder="ID를 입력해주세요"
-            autocomplete="email"
-            setInput={setEmail}
-          />
-          <LongInput
-            label="Password"
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
-            autocomplete="current-password"
-            setInput={setPassword}
-          />
-          <LoginButton />
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
+          <div>
+            <TextField
+              {...register("username")}
+              label="ID"
+              type="text"
+              id="username"
+              name="username"
+              placeholder="ID를 입력해주세요"
+              autoComplete="email"
+              margin="dense"
+              fullWidth
+            />
+          </div>
+          {inputErrors?.["username"] && (
+            <Box marginTop="10px" color={customColors.color_invalid}>
+              {inputErrors?.["username"].message}
+            </Box>
+          )}
+
+          <div>
+            <TextField
+              {...register("userPassword")}
+              label="Password"
+              type="password"
+              id="userPassword"
+              name="userPassword"
+              placeholder="비밀번호를 입력해주세요"
+              autoComplete="current-password"
+              margin="dense"
+              fullWidth
+            />
+
+            {inputErrors?.["userPassword"] && (
+              <Box marginTop="10px" color={customColors.color_invalid}>
+                {inputErrors?.["userPassword"].message}
+              </Box>
+            )}
+          </div>
+          <Button variant="contained" fullWidth type="submit">
+            로그인
+          </Button>
         </form>
-        {invalid ? (
-          <Box marginTop="10px" color={customColors.color_invalid}>
-            로그인 정보가 올바르지 않습니다.
-          </Box>
-        ) : null}
       </Box>
     </Box>
   );
