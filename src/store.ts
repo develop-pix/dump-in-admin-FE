f972c2efa5c39cc290066e17676bdd1c7430ff95
resetmerlin <resetmerlin@gmail.com>
2024-01-02 15:48:09 +0900

    Feat: AuthSlice Reducer 추가

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { api } from "./features";
+import { api, authSlice } from "./features";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    auth: authSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
