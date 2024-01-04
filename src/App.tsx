225defe4d757e7a174b7ab0ae83c1c47a23245e7
resetmerlin <resetmerlin@gmail.com>
2024-01-04 13:16:53 +0900

    Feat: Parent Route element에 RequireAuth 추가

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
   ReviewManage,
   UserManage,
 } from "./pages";
+import RequireAuth from "./components/auth/RequireAuth";
 
 const theme = createTheme({
   typography: {
@@ -26,16 +27,36 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/event" element={<EventManage />}></Route>
-          <Route path="/event/new" element={<EventNew />}></Route>
-          <Route path="/event/edit/:id" element={<EventEdit />}></Route>
-          <Route path="/branch/new" element={<PhotoboothNew />}></Route>
-          <Route path="/branch/edit/:id" element={<PhotoboothEdit />}></Route>
-          <Route path="/branch" element={<PhotoboothManage />}></Route>
-          <Route path="/user" element={<UserManage />}></Route>
-          <Route path="/pushmessage" element={<PushMessage />}></Route>
-          <Route path="/review" element={<ReviewManage />}></Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/event" element={<EventManage />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/event/new" element={<EventNew />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/event/edit/:id" element={<EventEdit />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/branch/new" element={<PhotoboothNew />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/branch/edit/:id" element={<PhotoboothEdit />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/branch" element={<PhotoboothManage />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/user" element={<UserManage />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/pushmessage" element={<PushMessage />}></Route>
+          </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/review" element={<ReviewManage />}></Route>
+          </Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
