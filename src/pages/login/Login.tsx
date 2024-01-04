a2ecc7481d5de4c1064ada2badfc66121657e327
resetmerlin <resetmerlin@gmail.com>
2024-01-04 13:06:30 +0900

    Feat: Login시 dashboard page에 가도록 hook 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,15 +1,19 @@
-import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { LoginInput } from "../../interface/reuse/Input.interface";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { requiredLogin } from "../../components/reuse/schema/loginSchema";
 import LoginForm from "../../components/login/LoginForm";
 import { useUserAuthenticatedMutation } from "../../features";
+import { useAppSelector } from "../../hooks";
+import { selectAuth } from "../../features/auth/authSlice";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const [usreAuthenticate, { error: loginError }] =
+  const navigate = useNavigate();
+  const auth = useAppSelector(selectAuth);
+  const [userAuthenticate, { error: loginError }] =
     useUserAuthenticatedMutation();
-
   const {
     register,
     handleSubmit,
@@ -23,8 +27,14 @@ export default function Login() {
   const onSubmitHandler: SubmitHandler<LoginInput> = async (
     data: LoginInput
   ) => {
-    usreAuthenticate({ data });
+    userAuthenticate(data);
   };
+
+  useEffect(() => {
+    if (Object.keys(auth).length !== 0) {
+      navigate("/dashboard");
+    }
+  }, [auth, navigate]);
 
   return (
     <>
