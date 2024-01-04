800cbd1d5a137983e77f68948700b4c75a122cde
resetmerlin <resetmerlin@gmail.com>
2024-01-04 13:32:46 +0900

    Feat: Login Page에 있는 비즈니스 로직을 새 Page hook에 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,50 +1,17 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-import { LoginInput } from "../../interface/reuse/Input.interface";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { requiredLogin } from "../../components/reuse/schema/loginSchema";
 import LoginForm from "../../components/login/LoginForm";
-import { useUserAuthenticatedMutation } from "../../features";
-import { useAppSelector } from "../../hooks";
-import { selectAuth } from "../../features/auth/authSlice";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLoginPage } from "./LoginPage.hook";
 
 export default function Login() {
-  const navigate = useNavigate();
-  const auth = useAppSelector(selectAuth);
-  const [userAuthenticate, { error: loginError }] =
-    useUserAuthenticatedMutation();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors: inputErrors },
-  } = useForm<LoginInput>({
-    mode: "onChange",
-    resolver: zodResolver(requiredLogin),
-  });
-
-  /*로그인 버튼 입력시*/
-  const onSubmitHandler: SubmitHandler<LoginInput> = async (
-    data: LoginInput
-  ) => {
-    userAuthenticate(data);
-  };
-
-  useEffect(() => {
-    if (Object.keys(auth).length !== 0) {
-      navigate("/dashboard");
-    }
-  }, [auth, navigate]);
+  const [onSubmitHandler, register, handleSubmit, inputErrors, loginError] =
+    useLoginPage();
 
   return (
-    <>
-      <LoginForm
-        onSubmitHandler={onSubmitHandler}
-        register={register}
-        handleSubmit={handleSubmit}
-        inputErrors={inputErrors}
-        loginError={loginError}
-      />
-    </>
+    <LoginForm
+      onSubmitHandler={onSubmitHandler}
+      register={register}
+      handleSubmit={handleSubmit}
+      inputErrors={inputErrors}
+      loginError={loginError}
+    />
   );
 }
