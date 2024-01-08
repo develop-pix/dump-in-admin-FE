4ce0f3938311ce1ef8d9028075f43383e484d925
resetmerlin <resetmerlin@gmail.com>
2024-01-08 12:50:04 +0900

    Feat: Dummy Dashboard 타입 추가

--- src/interface/dto/Dto.interface.ts
+++ src/interface/dto/Dto.interface.ts
@@ -4,3 +4,14 @@ export interface IUser {
   success: boolean;
   data: object;
 }
+
+export interface IDashboard {
+  code: number;
+  message: string;
+  success: boolean;
+  data: {
+    date: string;
+    user: number;
+    review: number;
+  };
+}
