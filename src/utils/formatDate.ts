94195301c8154c4aea9bdf43f68e41478e38f655
resetmerlin <resetmerlin@gmail.com>
2024-01-12 14:26:57 +0900

    Feat: 날짜를 year-month-day 형식으로 바꿔주는 함수 추가

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param dateString ISO 860 날짜, ex: "2023-12-29T15:00:00.000Z"
+ * @returns ISO 860 날짜를 "2023-12-29T15:00:00.000Z", 2023-12-29 형식으로 변경
+ */
+export function formatDate(dateString: string) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // months are 0-indexed
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
