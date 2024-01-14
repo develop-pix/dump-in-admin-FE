b52e244905b01f58023ddc66df12400f823a8532
resetmerlin <resetmerlin@gmail.com>
2024-01-14 19:39:21 +0900

    refactor: formatDate 함수 설명하는 주석 변경 및 유효한 형식의 문자열인지 체크하는 script 추가

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,12 @@
 /**
  *
- * @param dateString ISO 860 날짜, ex: "2023-12-29T15:00:00.000Z"
- * @returns ISO 860 날짜를 "2023-12-29T15:00:00.000Z", 2023-12-29 형식으로 변경
+ * @param dateString ISO 8601 날짜, ex: "2023-12-29T15:00:00.000Z"
+ * @returns 2023-12-29 형식으로 변경
  */
 export function formatDate(dateString: string) {
+  if (!/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(dateString)) {
+    throw new Error("Invalid ISO 8601 date format");
+  }
   const date = new Date(dateString);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0"); // months are 0-indexed
