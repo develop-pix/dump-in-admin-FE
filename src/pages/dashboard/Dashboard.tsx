4454927a3e7577d843ee9817bf3239e5cdb7e372
resetmerlin <resetmerlin@gmail.com>
2024-01-14 19:36:10 +0900

    refactor: 함수나 값을 설명하는 주석으로 변경

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -6,11 +6,10 @@ import DashBoardTable from "../../components/dashboard/DashBoardTable";
 import { formatDate } from "../../utils";
 import { useMemo } from "react";
 
-// 지점 관리 페이지
 export default function DashBoard() {
   const { data: dashboardData } = useGetDashboardQuery();
 
-  // 변경된 날짜 형식을 가지고 있는 데이터
+  /** 변경된 날짜 형식을 가지고 있는 데이터 */
   const filteredDateData = useMemo(
     () =>
       dashboardData?.data.map((x) => {
