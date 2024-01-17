f9cea97d245e3ea26454310c998aa0f1a9aaf8b3
resetmerlin <resetmerlin@gmail.com>
2024-01-17 15:22:29 +0900

    Feat: getEvent 쿼리 mutation 추가

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { isApiError, toErrorWithMessage } from "../../utils";
 import { setCredentials } from "../auth/authSlice";
-import { IDashboards, IUser } from "../../interface/dto/Dto.interface";
+import { IDashboards, IEvents, IUser } from "../../interface/dto/Dto.interface";
+import { eventsAdded } from "..";
 
 export const api = createApi({
   reducerPath: "api",
@@ -12,6 +13,28 @@ export const api = createApi({
   endpoints: (build) => ({
     getDashboard: build.query<IDashboards, void>({
       query: () => "api/dashboard",
+    }),
+    getEvent: build.mutation<IEvents, { page: number; perPage: number }>({
+      query: ({ page, perPage }) => `api/event?page=${page}&perPage=${perPage}`,
+
+      async onQueryStarted({ page, perPage }, api) {
+        const { dispatch, queryFulfilled } = api;
+
+        if (page && perPage)
+          try {
+            const { data: fetchedData } = await queryFulfilled;
+            dispatch(
+              eventsAdded({ page: page - 1, data: fetchedData?.data?.results })
+            );
+          } catch (error) {
+            if (isApiError(error)) {
+              const { data } = error.error;
+              reportError(data.message);
+            } else {
+              reportError(toErrorWithMessage(error).message);
+            }
+          }
+      },
     }),
     userAuthenticated: build.mutation<
       IUser,
@@ -46,4 +69,8 @@ export const api = createApi({
   }),
 });
 
-export const { useUserAuthenticatedMutation, useGetDashboardQuery } = api;
+export const {
+  useUserAuthenticatedMutation,
+  useGetDashboardQuery,
+  useGetEventMutation,
+} = api;
