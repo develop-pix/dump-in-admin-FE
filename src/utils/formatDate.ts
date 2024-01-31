49327c5d73cdc7a7deaeab59ba7f6920f76c57de
resetmerlin <resetmerlin@gmail.com>
2024-01-31 15:51:18 +0900

    Refactor: formatDate validation 삭제

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,9 +4,6 @@
  * @returns 2023-12-29 형식으로 변경
  */
 export function formatDate(dateString: string) {
-  if (!/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(dateString)) {
-    throw new Error("Invalid ISO 8601 date format");
-  }
   const date = new Date(dateString);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0"); // months are 0-indexed
