973b665e021bf15e02d122db4d3d11dd6ad74efc
resetmerlin <resetmerlin@gmail.com>
2024-01-31 16:07:49 +0900

    Feat: Event Manage 컴포넌트에 사용될 타입 추가

--- src/interface/EventManage.interface.ts
+++ src/interface/EventManage.interface.ts
@@ -1,4 +1,6 @@
 import { jsx } from "@emotion/react";
+import { EventsState } from "../features";
+import { IEvent } from "./dto/Dto.interface";
 
 export interface EventTableColumn {
   id: "image" | "name" | "title" | "description" | "term" | "hashtag" | "id";
@@ -6,4 +8,34 @@ export interface EventTableColumn {
   minWidth?: number;
   align?: "right" | "left" | "center" | "inherit" | "justify" | undefined;
   format?: (value: string) => jsx.JSX.Element;
+}
+
+export interface EventManageProps {
+  data: EventsState[];
+  page: number;
+  dataAfterSearch: IEvent[];
+  handlePageChange: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    newPage: number
+  ) => void;
+  handleSearchInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface EventManageTableContainerProps {
+  dataAfterSearch: IEvent[];
+  page: number;
+  handlePageChange: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    newPage: number
+  ) => void;
+}
+
+export interface EventTableRowProps {
+  id: number;
+  src: string;
+  name: string;
+  title: string;
+  description: string;
+  term: string;
+  hashtag: string[];
 }
